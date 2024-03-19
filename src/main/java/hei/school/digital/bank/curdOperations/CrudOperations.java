8414e8ea4f788fee52435c888b31bc22b886329f
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-03-19 23:21:25 +0300

    chore: add the findAll method to the CrudOperations class

--- src/main/java/hei/school/digital/bank/curdOperations/CrudOperations.java
+++ src/main/java/hei/school/digital/bank/curdOperations/CrudOperations.java
@@ -1,12 +1,15 @@
 package hei.school.digital.bank.curdOperations;
 
+import java.util.List;
+
 public interface CrudOperations<T , T_id>{
 
   T create(T entity);
+  List<T> findAll();
 
   T findById(T_id id);
 
-  T update(T_id id);
+  T update(T entity);
 
   void deleteById(T_id id);
 

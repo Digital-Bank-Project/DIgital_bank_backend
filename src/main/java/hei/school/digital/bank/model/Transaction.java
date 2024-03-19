24d88599e2f5beb6a1c89856182ba0af2c87723d
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-03-19 18:37:43 +0300

    feat: creating Transaction model

---
+++ src/main/java/hei/school/digital/bank/model/Transaction.java
@@ -0,0 +1,30 @@
+package hei.school.digital.bank.model;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transaction")
+public class Transaction {
+
+  @Id
+  private Long id;
+  private double amount;
+  private LocalDateTime dateTime;
+  private TransactionType type;
+  private Long accountId;
+
+
+  public enum TransactionType {
+    DEBIT, CREDIT
+  }
+
+}

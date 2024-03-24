79ee089e0530f0ca875f8410400fffffe34f0d88
= <=>
2024-03-24 13:14:36 +0300

    chore : updated account table

--- src/main/java/hei/school/digital/bank/Dbmigration/migration/create_table_account.sql
+++ src/main/java/hei/school/digital/bank/Dbmigration/migration/create_table_account.sql
@@ -1,13 +1,14 @@
 
-CREATE TABLE IF NOT EXISTS transfer (
-    transfer_id SERIAL PRIMARY KEY,
-    id_sender_account VARCHAR(50) NOT NULL,
-    id_receiver_account VARCHAR(50) NOT NULL,
-    transfer_amount DOUBLE PRECISION NOT NULL,
-    reason VARCHAR(200) NOT NULL,
-    effective_date TIMESTAMP NOT NULL,
-    registration_date TIMESTAMP NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    label VARCHAR(100) NOT NULL,
-    account_id INT NOT NULL
+CREATE TABLE IF NOT EXISTS account (
+    account_id SERIAL PRIMARY KEY,
+    last_name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    date_of_birth DATE CHECK (date_of_birth <= CURRENT_DATE - INTERVAL '23 years'),
+    monthly_salary_net DOUBLE PRECISION NOT NULL,
+    unique_account_number VARCHAR(10) NOT NULL,
+    overdraft_enabled BOOL NOT NULL,
+    bank VARCHAR(100) NOT NULL,
+    overdraft_interest_details FLOAT8 NOT NULL,
+    overdraft_start_date TIMESTAMP NOT NULL,
+    overdraft_interest_id INT NOT NULL
 );

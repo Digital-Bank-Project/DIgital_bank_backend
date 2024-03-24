bd9b3fe424074b350a1f69c0eb132e5aab404b83
= <=>
2024-03-24 13:54:11 +0300

    feat : Added test data to the account table

---
+++ src/main/java/hei/school/digital/bank/Dbmigration/testData/insert_account.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO account (last_name, first_name, date_of_birth, monthly_salary_net, unique_account_number, overdraft_enabled, bank, overdraft_interest_details, overdraft_start_date, overdraft_interest_id)
+VALUES
+('Doe', 'John', '1990-05-15', 3000.00, 'ABC123', TRUE, 'BMOI', 0.05, '2023-01-01', 1),
+('Smith', 'Alice', '1985-08-20', 4000.00, 'DEF456', FALSE, 'BOA', 0.07, '2022-12-01', 2),
+('Johnson', 'Emma', '1992-11-10', 2500.00, 'GHI789', TRUE, 'BOA', 0.06, '2023-03-15', 1),
+('Brown', 'Michael', '1988-04-25', 3500.00, 'JKL012', TRUE, 'BMOI', 0.04, '2022-11-01', 3),
+('Taylor', 'Sophia', '1995-09-30', 2800.00, 'MNO345', FALSE, 'BMOI', 0.08, '2023-02-01', 2);

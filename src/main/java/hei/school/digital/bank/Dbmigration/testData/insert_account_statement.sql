ccd41e9870047b09859fea074315d6277f87d411
= <=>
2024-03-24 13:58:15 +0300

    feat : Inserted account_statement table data

---
+++ src/main/java/hei/school/digital/bank/Dbmigration/testData/insert_account_statement.sql
@@ -0,0 +1,7 @@
+INSERT INTO account_statement (account_id, start_date, end_date, motive, balance_principal, credit_mga, debit_mga)
+ VALUES
+(1, '2024-01-01', '2024-01-31', 'Monthly statement', 1000.00, 500.00, 0.00),
+(2, '2024-01-01', '2024-01-31', 'Monthly statement', 1500.00, 700.00, 200.00),
+(3, '2024-01-01', '2024-01-31', 'Monthly statement', 2000.00, 900.00, 300.00),
+(4, '2024-01-01', '2024-01-31', 'Monthly statement', 2500.00, 1200.00, 400.00),
+(5, '2024-01-01', '2024-01-31', 'Monthly statement', 3000.00, 1500.00, 500.00);

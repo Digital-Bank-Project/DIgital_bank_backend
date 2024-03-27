0f7f4e9c54f6b1c2b54b13245b2d7fc109fdc089
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-03-27 10:13:47 +0300

    refactor: modifying the repository transaction

--- src/main/java/hei/school/digital/bank/repository/TransactionRepository.java
+++ src/main/java/hei/school/digital/bank/repository/TransactionRepository.java
@@ -9,7 +9,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
@@ -19,12 +26,13 @@ public class TransactionRepository implements CrudOperations<Transaction,Long> {
   @Override
   public Transaction create(Transaction transaction) {
     try (Connection connection = PostgresDbConnection.getConnection()) {
-      String sql = "INSERT INTO transaction (amount, date_time, type, account_id) VALUES (?, ?, ?, ?)";
+      String sql = "INSERT INTO transaction (amount, motive, dateTime, type, accountId) VALUES (?, ?, ?, ?, ?)";
       PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
       statement.setDouble(1, transaction.getAmount());
-      statement.setTimestamp(2, Timestamp.valueOf(transaction.getDateTime()));
-      statement.setString(3, transaction.getType().toString());
-      statement.setLong(4, transaction.getAccountId());
+      statement.setString(2,transaction.getMotive());
+      statement.setTimestamp(3, Timestamp.valueOf(transaction.getDateTime()));
+      statement.setString(4, transaction.getType().toString());
+      statement.setLong(5, transaction.getAccountId());
 
       int rowsInserted = statement.executeUpdate();
 
@@ -59,6 +67,7 @@ public class TransactionRepository implements CrudOperations<Transaction,Long> {
         Transaction transaction = new Transaction();
         transaction.setId(resultSet.getLong("id"));
         transaction.setAmount(resultSet.getDouble("amount"));
+        transaction.setMotive(resultSet.getString("motive"));
         transaction.setDateTime(resultSet.getTimestamp("dateTime").toLocalDateTime());
         transaction.setType(Transaction.TransactionType.valueOf(resultSet.getString("type")));
         transaction.setAccountId(resultSet.getLong("accountId"));
@@ -86,6 +95,7 @@ public class TransactionRepository implements CrudOperations<Transaction,Long> {
         transaction = new Transaction();
         transaction.setId(resultSet.getLong("id"));
         transaction.setAmount(resultSet.getDouble("amount"));
+        transaction.setMotive(resultSet.getString("motive"));
         transaction.setDateTime(resultSet.getTimestamp("date_time").toLocalDateTime());
         transaction.setType(Transaction.TransactionType.valueOf(resultSet.getString("type")));
         transaction.setAccountId(resultSet.getLong("account_id"));
@@ -100,13 +110,14 @@ public class TransactionRepository implements CrudOperations<Transaction,Long> {
   public Transaction update(Transaction entity) {
     try (Connection connection = PostgresDbConnection.getConnection();
          PreparedStatement preparedStatement = connection.prepareStatement(
-             "UPDATE transaction SET amount = ?, dateTime = ?, type = ?, accountId = ? WHERE id = ?")) {
+             "UPDATE transaction SET amount = ?, motive = ?, dateTime = ?, type = ?, accountId = ? WHERE id = ?")) {
 
       preparedStatement.setDouble(1, entity.getAmount());
-      preparedStatement.setTimestamp(2, Timestamp.valueOf(entity.getDateTime()));
-      preparedStatement.setString(3, entity.getType().toString());
-      preparedStatement.setLong(4, entity.getAccountId());
-      preparedStatement.setLong(5, entity.getId());
+      preparedStatement.setString(2,entity.getMotive());
+      preparedStatement.setTimestamp(3, Timestamp.valueOf(entity.getDateTime()));
+      preparedStatement.setString(4, entity.getType().toString());
+      preparedStatement.setLong(5, entity.getAccountId());
+      preparedStatement.setLong(6, entity.getId());
 
       int rowsUpdated = preparedStatement.executeUpdate();
 
@@ -126,7 +137,55 @@ public class TransactionRepository implements CrudOperations<Transaction,Long> {
 
 
   @Override
-  public void deleteById(Long aLong) {
+  public void deleteById(Long id) {
+    try (Connection connection = PostgresDbConnection.getConnection()) {
+      String sql = "DELETE FROM transaction WHERE id = ?";
+      PreparedStatement statement = connection.prepareStatement(sql);
+      statement.setLong(1, id);
+      statement.executeUpdate();
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+  }
 
+  public List<Transaction> getTransactionsByDateRange(Date startDate, Date endDate) {
+    List<Transaction> transactions = new ArrayList<>();
+
+    try (Connection connection = PostgresDbConnection.getConnection()) {
+      // Convertir la date en LocalDate
+      Instant startInstant = startDate.toInstant();
+      ZonedDateTime startZonedDateTime = startInstant.atZone(ZoneId.systemDefault());
+      LocalDate localStartDate = startZonedDateTime.toLocalDate();
+
+      Instant endInstant = endDate.toInstant();
+      ZonedDateTime endZonedDateTime = endInstant.atZone(ZoneId.systemDefault());
+      LocalDate localEndDate = endZonedDateTime.toLocalDate();
+
+      // Convertir LocalDate en LocalDateTime avec minuit comme heure
+      LocalDateTime startDateTime = localStartDate.atStartOfDay();
+      LocalDateTime endDateTime = localEndDate.atTime(LocalTime.MAX);
+
+      String sql = "SELECT * FROM transaction WHERE dateTime BETWEEN ? AND ?";
+      PreparedStatement statement = connection.prepareStatement(sql);
+      statement.setTimestamp(1, Timestamp.valueOf(startDateTime));
+      statement.setTimestamp(2, Timestamp.valueOf(endDateTime));
+
+      ResultSet resultSet = statement.executeQuery();
+
+      while (resultSet.next()) {
+        Transaction transaction = new Transaction();
+        transaction.setId(resultSet.getLong("id"));
+        transaction.setAmount(resultSet.getDouble("amount"));
+        transaction.setMotive(resultSet.getString("motive"));
+        transaction.setDateTime(resultSet.getTimestamp("dateTime").toLocalDateTime());
+        transaction.setType(Transaction.TransactionType.valueOf(resultSet.getString("type")));
+        transaction.setAccountId(resultSet.getLong("accountId"));
+        transactions.add(transaction);
+      }
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+
+    return transactions;
   }
 }

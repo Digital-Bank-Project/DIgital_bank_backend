f34accf00123373ae87dddd26f8e9d15743a1be9
Sarobidy-nantenaina <sarobidyrakotondrazafy1@gmail.com>
2024-03-27 10:38:42 +0300

    refactor: updating account service

--- src/main/java/hei/school/digital/bank/service/AccountService.java
+++ src/main/java/hei/school/digital/bank/service/AccountService.java
@@ -1,8 +1,14 @@
 package hei.school.digital.bank.service;
 
+import hei.school.digital.bank.exception.InsufficientFundsException;
 import hei.school.digital.bank.model.Account;
+import hei.school.digital.bank.model.BalanceHistory;
 import hei.school.digital.bank.repository.AccountRepository;
 import hei.school.digital.bank.utils.AccountNumberGenerator;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,12 +18,26 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
   private final AccountRepository accountRepository;
+  private final BalanceHistoryService balanceHistoryService;
 
   public Account createAccount(Account account) {
     String accountNumber = AccountNumberGenerator.generateAccountNumber();
     account.setAccountNumber(accountNumber);
+    Account createdAccount = accountRepository.create(account);
 
-    return accountRepository.create(account);
+
+    if (createdAccount != null) {
+
+      BalanceHistory initialBalanceHistory = new BalanceHistory();
+      initialBalanceHistory.setAccountId(createdAccount.getId());
+      initialBalanceHistory.setPrincipalBalance(createdAccount.getPrincipalBalance());
+      initialBalanceHistory.setLoanAmount(0);
+      initialBalanceHistory.setInterestAmount(0);
+      initialBalanceHistory.setTimestamp( LocalDateTime.now());
+      balanceHistoryService.createBalanceHistory(initialBalanceHistory);
+    }
+    return createdAccount;
+
   }
 
   public List<Account> getAllAccounts(){
@@ -29,16 +49,22 @@ public class AccountService {
   }
 
   public Account updateAccount(Account account) {
-    Account accountToUpdate = accountRepository.findById(account.getId());
+    Account updatedAccount = accountRepository.findById(account.getId());
 
-    if (accountToUpdate != null) {
-      return accountRepository.update(account);
+    if (updatedAccount != null) {
+       accountRepository.update(account);
+      BalanceHistory updatedBalanceHistory = new BalanceHistory();
+      updatedBalanceHistory.setAccountId(updatedAccount.getId());
+      updatedBalanceHistory.setPrincipalBalance(updatedAccount.getPrincipalBalance());
+      updatedBalanceHistory.setTimestamp(LocalDateTime.now());
+      balanceHistoryService.createBalanceHistory(updatedBalanceHistory);
+
+      return updatedAccount;
     } else {
       System.out.println("Account with ID " + account.getId() + " not found.");
       return null;
     }
   }
-
 
   public void deleteAccountById(Long id) {
     accountRepository.deleteById(id);
